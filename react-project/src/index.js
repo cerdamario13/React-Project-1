31263c0d7ed4150620fffdb2aa770cabbb075328
cerdamario13 <mario@wrangleworks.com>
2022-08-29 16:53:06 -0500

    started components

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import App from './App'; // This is a component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
