55d5cf3d8fedd4d376ca7686d27345c8c3f5cb06
cerdamario13 <mario@wrangleworks.com>
2022-08-30 15:27:52 -0500

    more components

--- react-project/src/components/ExpenseItem.js
+++ react-project/src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    
   return (
     <div className='expense-item'>
-      <div>March 29th 2022</div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$133.33</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
